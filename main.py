c95e9d9a7375936fad102c48733d76f098453d6f
Komandir2002 <temirlanlive2002@gmail.com>
2022-06-17 17:55:46 +0600

    hw1

---
+++ main.py
@@ -0,0 +1,18 @@
+#HW:
+# Создать пакет - (cars)
+# создать модули такие как (create_car, get_car_info)
+# create_cars —> Создать Абстрактный класс (Car)
+# Создать методы для Car - (start_engine, stop_engine)
+# get_car_info —> def get_car_info(car): —> car_info
+# создать main.py файл и там использовать ваш пакет (cars)
+
+from ters import Car
+
+
+bmw = Car('BMW', 'e34', 'black',8,260)
+
+bmw.get_info()
+bmw.start_engine()
+bmw.stop_engine()
+
+
---
+++ ters.py
@@ -0,0 +1,23 @@
+
+class Car:
+
+    def __init__(self, title, model, color, engine, tachometer):
+        self.title = title
+        self.model = model
+        self.color = color
+        self.engine = engine
+        self.tachometer = tachometer
+
+    def start_engine(self):
+        print(f"On {self.title} {self.model} engine starting!")
+
+    def stop_engine(self):
+        print(f"Off {self.title} {self.model} engine stopping!")
+
+    def get_info(self):
+        print(f'title: {self.title},'
+              f'model: {self.model},'
+              f'color: {self.color},'
+              f'engine: {self.engine},'
+              f'tachometer: {self.tachometer}')
+
